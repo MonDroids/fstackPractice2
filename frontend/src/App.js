6b0259d60ab50cc34126cebf4c8a3234557b693b
SainDev <46877765+MonDroids@users.noreply.github.com>
2025-08-18 16:20:59 +0800

    updt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 
 function App(){
   const [users, setUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
   // Backend-с хэрэглэгчийн мэдээлэл авах
   useEffect(() => {
     fetch("http://localhost:3002/users")
@@ -10,8 +13,50 @@ function App(){
       .catch((error) => console.error("Error fetching users:", error));
   }, []);
 
+  // Хэрэглэгч нэмэх
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const newUser = {name, email};
+    fetch('http://localhost:3002/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers((prevUsers) => [...prevUsers, data]);
+        setName("");
+        setEmail("");
+      })
+      .catch((error) => console.error("Error adding user:", error));
+  };
+  
+
   return (
     <div>
+      <h1>Шинэ хэрэглэгч нэмэх</h1>
+      
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Нэр"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <input
+          type="email"
+          placeholder="И-мэйл"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <button type="submit">Нэмэх</button>
+      </form>
+
+      
       <h1>Хэрэглэгчидийн мэдээллийг харах</h1>
       <ul>
         {users.map((user) => (
